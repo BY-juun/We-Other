6f5e3ad9ebde72c1fe4ecc130dc3db5e4875dd59
ByeongJun <neostgeart@ajou.ac.kr>
2022-04-15 23:13:23 +0900

    Update Login Key

--- client/API/User/index.ts
+++ client/API/User/index.ts
@@ -1,13 +1,15 @@
 import axios from "axios";
 import { LoginData, SignUpData } from "Types/User";
+import { targetUrl } from "Utils/targetUrl";
 import { customAxios } from "../../Utils/customAxios";
 
 export const SignUpAPI = async (reqData: SignUpData) => {
-	const { data } = await customAxios.post(`/user/sign-up`, reqData);
-	return data;
+  console.log("dd");
+  const { data } = await customAxios.post(`/user/sign-up`, reqData);
+  return data;
 };
 
 export const LoginAPI = async (reqData: LoginData) => {
-	const { data } = await customAxios.post(`/user/sign-in`, reqData);
-	return data;
+  const { data } = await customAxios.post(`/user/sign-in`, reqData);
+  return data;
 };
