9e68cac94af841ddb8dc2f49eb4b52377cef8786
ByeongJun <neostgeart@ajou.ac.kr>
2022-04-16 02:56:33 +0900

    Feat getUserInfoQuery

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -30,8 +30,6 @@ const WeOther = ({ Component, pageProps }: AppProps) => {
 WeOther.getInitialProps = async (appContext: AppContext) => {
   const { ctx } = appContext;
   const allCookies = cookies(ctx);
-  console.log(allCookies);
-
   const accessTokenByCookie = allCookies["accessToken"];
   const userIdx = allCookies["userIdx"];
   if (accessTokenByCookie !== undefined && userIdx !== undefined) {
